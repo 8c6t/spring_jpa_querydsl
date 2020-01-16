d93fe113a654fe2dbf568ff1ae8e8f15c0a0d033
8c6t <nkw8620@gmail.com>
2020-01-16 14:44:07 +0900

    1-2. Querydsl 설정과 검증

--- src/test/java/com/hachicore/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/hachicore/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,37 @@
 package com.hachicore.querydsl;
 
+import com.hachicore.querydsl.entity.Hello;
+import com.hachicore.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query.selectFrom(qHello)
+                .fetchOne();
+
+        assertEquals(hello, result);
+        assertEquals(hello.getId(), result.getId());
     }
 
 }
