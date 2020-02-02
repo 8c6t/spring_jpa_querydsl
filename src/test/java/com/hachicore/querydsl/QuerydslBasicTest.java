39477006fa771c7f632bafeb36067ce77d445576
8c6t <nkw8620@gmail.com>
2020-02-02 22:26:30 +0900

    3-11. 서브 쿼리

--- src/test/java/com/hachicore/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/hachicore/querydsl/QuerydslBasicTest.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 import static com.hachicore.querydsl.entity.QMember.member;
 import static com.hachicore.querydsl.entity.QTeam.team;
+import static com.querydsl.jpa.JPAExpressions.select;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -314,6 +315,44 @@ public class QuerydslBasicTest {
 
         boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
         assertThat(loaded).as("페치 조인 적용").isTrue();
+
+    }
+
+    /**
+     * 나이가 평균 이상인 회원
+     */
+    @Test
+    void subQuery() {
+        QMember memberSub = new QMember("membserSub");
+
+            List<Member> result = queryFactory
+                    .selectFrom(member)
+                    .where(member.age.eq(
+                            select(memberSub.age.max())
+                                    .from(memberSub)
+                                    .where(memberSub.age.gt(10))
+                    ))
+                    .fetch();
+
+            assertThat(result).extracting("age")
+                    .containsExactly(20, 30, 40);
+
+    }
+
+    @Test
+    void selectSubQuery() {
+        QMember memberSub = new QMember("membserSub");
+
+        List<Tuple> result = queryFactory
+                .select(member.username,
+                    select(memberSub.age.avg())
+                        .from(memberSub))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple : " + tuple);
+        }
     }
 
 }
