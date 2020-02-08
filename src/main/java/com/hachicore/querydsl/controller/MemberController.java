b4847ff41b7a8da9a8a6434a4acc3e55be3725f0
8c6t <nkw8620@gmail.com>
2020-02-08 15:25:13 +0900

    5-4. 조회 API 컨트롤러 개발

---
+++ src/main/java/com/hachicore/querydsl/controller/MemberController.java
@@ -0,0 +1,23 @@
+package com.hachicore.querydsl.controller;
+
+import com.hachicore.querydsl.dto.MemberSearchCondition;
+import com.hachicore.querydsl.dto.MemberTeamDto;
+import com.hachicore.querydsl.repository.MemberJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+
+}
