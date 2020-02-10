b216d16a283510f37fc426438122dbf9f362b79b
8c6t <nkw8620@gmail.com>
2020-02-10 20:28:32 +0900

    7-4. Querydsl 지원 클래스 직접 만들기

--- src/main/java/com/hachicore/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/com/hachicore/querydsl/repository/MemberRepositoryImpl.java
@@ -6,13 +6,11 @@ import com.hachicore.querydsl.dto.QMemberTeamDto;
 import com.hachicore.querydsl.entity.Member;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.data.repository.support.PageableExecutionUtils;
 
 import javax.persistence.EntityManager;
@@ -21,37 +19,17 @@ import java.util.List;
 import static com.hachicore.querydsl.entity.QMember.member;
 import static com.hachicore.querydsl.entity.QTeam.team;
 import static org.springframework.util.StringUtils.hasText;
-public class MemberRepositoryImpl extends QuerydslRepositorySupport implements MemberRepositoryCustom {
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
+
 
     private final JPAQueryFactory queryFactory;
 
-//    public MemberRepositoryImpl(EntityManager em) {
-//        this.queryFactory = new JPAQueryFactory(em);
-//    }
     public MemberRepositoryImpl(EntityManager em) {
-        super(Member.class);
         this.queryFactory = new JPAQueryFactory(em);
     }
 
     @Override
     public List<MemberTeamDto> search(MemberSearchCondition condition) {
-
-//        List<MemberTeamDto> result = from(member)
-//                .leftJoin(member.team, team)
-//                .where(
-//                        usernameEq(condition.getUsername()),
-//                        teamNameEq(condition.getTeamName()),
-//                        ageGoe(condition.getAgeGoe()),
-//                        ageLoe(condition.getAgeLoe())
-//                )
-//                .select(new QMemberTeamDto(
-//                        member.id.as("memberId"),
-//                        member.username,
-//                        member.age,
-//                        team.id.as("teamId"),
-//                        team.name.as("teamName")))
-//                .fetch();
-
         return queryFactory
                 .select(new QMemberTeamDto(
                         member.id.as("memberId"),
@@ -95,27 +73,6 @@ public class MemberRepositoryImpl extends QuerydslRepositorySupport implements M
         Long total = results.getTotal();
 
         return new PageImpl<>(content, pageable, total);
-    }
-
-    @Override
-    public Page<MemberTeamDto> searchPageSimple2(MemberSearchCondition condition, Pageable pageable) {
-        JPQLQuery<MemberTeamDto> jpaQuery = from(member)
-                .leftJoin(member.team, team)
-                .where(
-                        usernameEq(condition.getUsername()),
-                        teamNameEq(condition.getTeamName()),
-                        ageGoe(condition.getAgeGoe()),
-                        ageLoe(condition.getAgeLoe())
-                )
-                .select(new QMemberTeamDto(
-                        member.id.as("memberId"),
-                        member.username,
-                        member.age,
-                        team.id.as("teamId"),
-                        team.name.as("teamName")));
-
-        JPQLQuery<MemberTeamDto> query = getQuerydsl().applyPagination(pageable, jpaQuery);
-        List<MemberTeamDto> content = query.fetch();
     }
 
     @Override
